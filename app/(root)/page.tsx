151c15256a9cce2e34496015141775e40ba2b6e0
vergarapog <vbrian.dev@gmail.com>
2024-06-08 18:47:11 +0800

    addtl changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,30 @@
+import HeaderBox from "@/components/ui/HeaderBox";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Brian" };
+
+  return (
+    <div className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
